3abb83ca40f14627c8aa6b9dedb85cc8e2fec290
phanluutienquang <quangvip2081999@gmail.com>
2025-05-25 10:00:58 +0700

    cap nhat

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -27,5 +27,12 @@ namespace WebBanHang.Controllers
         {
             return View("~/Views/Checkout/Index.cshtml");
         }
+        public async Task<IActionResult> Add(long Id)
+        {
+            ProductModel product = await _dataContext.Products.FindAsync(Id);
+            List<CartItemModel> cart = HttpContext.Session.GetJson<List<CartItemModel>>("Cart") ?? new List<CartItemModel>();
+            CartItemModel cartItems = cart.Where(c => c.ProductId == Id).FirstOrDefault();
+
+        }
     }
 }
--- obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d99efdae3f0e348ae6b7c53a8525cc047b89bf57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ef27572a6f2860cdd5f399ad57b56a7eecf669")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
