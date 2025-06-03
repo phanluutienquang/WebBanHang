271ebd8a33e375e2fc63612957f74ba4360660c4
phanluutienquang <quangvip2081999@gmail.com>
2025-06-04 00:00:21 +0700

    Update

--- Models/CategoryModel.cs
+++ Models/CategoryModel.cs
@@ -6,9 +6,9 @@ namespace WebBanHang.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required,MinLength(4,ErrorMessage ="Yêu cầu nhập Tên Danh Mục")]
+        [Required(ErrorMessage ="Yêu cầu nhập Tên Danh Mục")]
         public string Name { get; set; }
-        [Required, MinLength(4, ErrorMessage = "Yêu cầu nhập Mô Tả Danh Mục")]
+        [Required( ErrorMessage = "Yêu cầu nhập Mô Tả Danh Mục")]
         public string Description { get; set; }
         public string Slug { get; set; }
         public int Status { get; set; }
--- obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8097934c02fe314e3686f60affcfbbce293472fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f31ca63ccfe972c4caa3cc800e416eaa86675868")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
