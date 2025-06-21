ba10043ecded816821e18c04fb79b624195c1317
phanluutienquang <quangvip2081999@gmail.com>
2025-06-22 00:29:25 +0700

    quangplt: initial commit

--- Areas/Admin/Controllers/UserController.cs
+++ Areas/Admin/Controllers/UserController.cs
@@ -13,10 +13,10 @@ namespace WebBanHang.Areas.Admin.Controllers
     {
         private readonly UserManager<AppUserModel> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
-        
-        public UserController(UserManager<AppUserModel> userManager,RoleManager<IdentityRole> roleManager)
+
+        public UserController(UserManager<AppUserModel> userManager, RoleManager<IdentityRole> roleManager)
         {
-            
+
             _userManager = userManager;
             _roleManager = roleManager;
         }
@@ -34,5 +34,69 @@ namespace WebBanHang.Areas.Admin.Controllers
             ViewBag.Roles = new SelectList(roles, "Id", "Name");
             return View(new AppUserModel());
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("Create")]
+
+        public async Task<IActionResult> Create(AppUserModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                var createUserResult = await _userManager.CreateAsync(user,user.PasswordHash);
+                if (createUserResult.Succeeded) 
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    foreach (var error in createUserResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View(user);
+                }    
+            }
+            else
+            {
+                TempData["error"] = "Model có vài thứ bị lỗi";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+            var roles = await _roleManager.Roles.ToListAsync();
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+            return View(user);
+        }
+       
+        [HttpGet]
+        [Route("Delete")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }    
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) 
+            {
+                return NotFound();
+            }
+            var deleteResult = await _userManager.DeleteAsync(user);
+            if (!deleteResult.Succeeded)
+            {
+                return View("Eror");
+            }
+            TempData["success"] = "User đã được xoá thành công";
+            return RedirectToAction("Index");
+        }
+
     }
 }
