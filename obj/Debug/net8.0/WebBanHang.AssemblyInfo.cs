e1c27d48e22e3d77c83afea989705198df6f51e8
phanluutienquang <quangvip2081999@gmail.com>
2025-07-02 00:11:37 +0700

    quangplt: initial commit

--- obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f31f466d6a6628ce355f49433cba6d77c54c59ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8d596dccb0c01de9d725aee3034b5f95587aead")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
